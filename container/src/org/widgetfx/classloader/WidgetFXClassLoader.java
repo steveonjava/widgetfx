d2bdd51eb05afdce757a8c00194019baeb1f3f7a
keith@widgetfx.org <keith@widgetfx.org@a410fa20-6b51-0410-bf03-e7c14b531842>
2009-04-02 03:03:31 +0000

    Security framework improvements.

--- container/src/org/widgetfx/classloader/WidgetFXClassLoader.java
+++ container/src/org/widgetfx/classloader/WidgetFXClassLoader.java
@@ -4,13 +4,23 @@
  */
 package org.widgetfx.classloader;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLStreamHandlerFactory;
 import java.security.AllPermission;
 import java.security.CodeSource;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PermissionCollection;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Keith Combs
@@ -32,15 +42,47 @@ public class WidgetFXClassLoader extends URLClassLoader {
 
     @Override
     protected PermissionCollection getPermissions(CodeSource codesource) {
-        System.out.println("getPermissions called with codesigners: " + (codesource.getCodeSigners() == null ? null : Arrays.asList(codesource.getCodeSigners())));
-        System.out.println("getPermissions called with codecertificates: " + (codesource.getCertificates() == null ? null : Arrays.asList(codesource.getCertificates())));
-        if (codesource.getCertificates() != null && codesource.getCertificates().length > 0) {
-            AllPermission all = new AllPermission();
-            PermissionCollection permissions = all.newPermissionCollection();
-            permissions.add(all);
-            return permissions;
-        } else {
-            return super.getPermissions(codesource);
+        try {
+            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+            String trustedCerts = System.getProperty("deployment.user.security.trusted.certs");
+            System.out.println(trustedCerts);
+            FileInputStream fis = new FileInputStream(trustedCerts);
+            try {
+                ks.load(fis, null);
+            } finally {
+                fis.close();
+            }
+            Certificate[] certificates = codesource.getCertificates();
+            if (certificates != null && certificates.length > 0) {
+                for (Certificate cert : certificates) {
+                    String alias = ks.getCertificateAlias(cert);
+                    boolean trusted = alias != null && ks.isCertificateEntry(alias);
+                    if (!trusted) {
+                        alias = "deploymentusercert$tsflag-" + System.currentTimeMillis();
+                        ks.setCertificateEntry(alias, cert);
+                        System.out.println("storing key with alias = " + alias);
+                        FileOutputStream fos = new FileOutputStream(trustedCerts);
+                        try {
+                            ks.store(fos, new char[0]);
+                        } finally {
+                            fos.close();
+                        }
+                    }
+                }
+                AllPermission all = new AllPermission();
+                PermissionCollection permissions = all.newPermissionCollection();
+                permissions.add(all);
+                return permissions;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(WidgetFXClassLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(WidgetFXClassLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (CertificateException ex) {
+            Logger.getLogger(WidgetFXClassLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (KeyStoreException ex) {
+            Logger.getLogger(WidgetFXClassLoader.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return super.getPermissions(codesource);
     }
 }
